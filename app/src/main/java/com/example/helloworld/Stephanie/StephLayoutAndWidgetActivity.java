48e72295b6440a7d0c14480f893b1b3024fee515
owen <ozqowen@163.com>
2019-08-08 22:57:23 +0800

    owen_class_four:Activity-生命周期，启动模式

--- app/src/main/java/com/example/helloworld/Stephanie/StephLayoutAndWidgetActivity.java
+++ app/src/main/java/com/example/helloworld/Stephanie/StephLayoutAndWidgetActivity.java
@@ -3,14 +3,18 @@ package com.example.helloworld.Stephanie;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.helloworld.R;
 
 public class StephLayoutAndWidgetActivity extends AppCompatActivity {
 
+    private static final String TAG = "StephLayoutAndWidgetAct";
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_steph_layout_and_widget);
+        Log.d(TAG, "onCreate: " + TAG);
     }
 }
