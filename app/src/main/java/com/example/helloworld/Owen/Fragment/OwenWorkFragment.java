4cfcd78caa5dbc7139ff8632cc15e3d457a57252
owen <ozqowen@163.com>
2019-08-18 15:45:28 +0800

    owen_class_eight:Fragment的基本用法;

---
+++ app/src/main/java/com/example/helloworld/Owen/Fragment/OwenWorkFragment.java
@@ -0,0 +1,23 @@
+package com.example.helloworld.Owen.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.helloworld.R;
+
+public class OwenWorkFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_owen_work, container, false);
+        return view;
+    }
+}
